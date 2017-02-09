d2992c7f8c7344b75ca5175f079108288fcc2660
andrewhoon <hoonandrew1709@gmail.com>
2017-02-09 10:17:54 -0500

    first commit

---
+++ Migrations/Models/GameScore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Migrations.Models
+{
+    public class GameScore
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+    }
+}
